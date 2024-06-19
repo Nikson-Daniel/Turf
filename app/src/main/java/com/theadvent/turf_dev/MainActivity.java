0852720a7f211a10df46e70ef0c6012560f00a2f
Nikson-Daniel <niksonthedaniel7777@gmail.com>
2024-06-19 21:52:11 +0530

    Auth UI updates

---
+++ app/src/main/java/com/theadvent/turf_dev/MainActivity.java
@@ -0,0 +1,28 @@
+package com.theadvent.turf_dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            loadFragment(new LoginFragment());
+        }
+
+    }
+
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
